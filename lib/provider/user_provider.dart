10ad2a88ae8524d300fa88e64b727f216bbc439b
Ibrahim Imam <ibrhmmm552@gmail.com>
2025-04-19 12:33:22 +0700

    handle shared preferences

--- lib/provider/user_provider.dart
+++ lib/provider/user_provider.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserProvider extends ChangeNotifier {
   bool isGuru = false;
 
-  void setIsGuru(bool statusLogin) {
+  UserProvider() {
+    _loadUserState();
+  }
+
+  // Load state from SharedPreferences
+  Future<void> _loadUserState() async {
+    final prefs = await SharedPreferences.getInstance();
+    isGuru = prefs.getBool('isGuru') ?? false;
+    notifyListeners();
+  }
+
+  void setIsGuru(bool statusLogin) async {
     isGuru = statusLogin;
+
+    // Save to SharedPreferences whenever state changes
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isGuru', statusLogin);
+
     notifyListeners();
   }
 }
